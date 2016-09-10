6027574f58cc1c90478da2da976c242e5283fec1
Administrator <pengweiyuang@gmail.com>
2016-09-10 12:37:04 +0800

    增加cache

---
+++ junhuang-core/src/main/java/com/junhuang/market/core/common/CacheConstants.java
@@ -0,0 +1,8 @@
+package com.junhuang.market.core.common;
+
+/**
+ * Created by Administrator on 2016/9/10 0010.
+ */
+public class CacheConstants {
+    public static final String USER_DATA_CACHE="user_data_cache";
+}
--- junhuang-core/src/main/java/com/junhuang/market/core/repository/UserRepository.java
+++ junhuang-core/src/main/java/com/junhuang/market/core/repository/UserRepository.java
@@ -1,6 +1,8 @@
 package com.junhuang.market.core.repository;
 
+import com.junhuang.market.core.common.CacheConstants;
 import com.junhuang.market.core.domain.User;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
@@ -12,9 +14,11 @@ import java.util.List;
  */
 @Repository
 public interface UserRepository extends MongoRepository<User, String> {
-    List<User> findByUserNameAndPassword(String userName,String password);
+
+    List<User> findByUserNameAndPassword(String userName, String password);
 
     User findByUserName(String userName);
 
+    @Cacheable(value = CacheConstants.USER_DATA_CACHE, key = "#email")
     User findByEmail(String email);
 }
